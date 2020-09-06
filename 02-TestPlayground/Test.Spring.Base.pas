e9182d1f4b9df65a259abc1bcedc077f766dbcf2
Bogdan Polak <bogdan.polak@bsc.com.pl>
2020-09-07 00:04:33 +0200

    Separated Spring.Base unit tests

--- 02-TestPlayground/Test.Spring.Base.pas
+++ 02-TestPlayground/Test.Spring.Base.pas
@@ -6,14 +6,17 @@ uses
   DUnitX.TestFramework;
 
 type
-
+  {$M+}
   [TestFixture]
   TMyTestObject = class(TObject)
   public
     // [Setup] procedure Setup;
     // [TearDown] procedure TearDown;
   published
-    procedure Test1;
+    procedure Test_Spring_Nullable;
+    procedure Test_Tuples;
+    procedure Test_Array;
+    procedure Test_Vector;
   end;
 
 implementation
@@ -42,37 +45,61 @@ begin
     Result := Result + ']';
 end;
 
-procedure TMyTestObject.Test1;
+procedure TMyTestObject.Test_Spring_Nullable;
 var
   aVaule1: Nullable<TStringList>;
   aVaule2: Nullable<TStringList>;
-  sl1: TStringList;
-  aTupleISD: Tuple<Integer, String, Double>;
-  arr1: TArray<Integer>;
-  arr2: TArray<Integer>;
-  aIntVect: Vector<Integer>;
-  aVect2: TIntVector;
+  sl2: TStringList;
 begin
   aVaule1 := TStringList.Create();
   aVaule1.Value.Add('Aaaaa');
-  Assert.AreEqual(1, aVaule1.Value.Count);
   Assert.AreEqual(False, aVaule2.HasValue);
-  sl1 := aVaule2.GetValueOrDefault;
-  Assert.IsNull(sl1);
+  sl2 := aVaule2.GetValueOrDefault;
 
-  // Guard.CheckNotNull(sl1,'sl1');
+  Assert.AreEqual(1, aVaule1.Value.Count);
+  Assert.IsTrue(aVaule1.HasValue);
+  Assert.IsNull(sl2);
+  // Guard.CheckNotNull(sl2,'"sl2" is null');
+  // Guard.CheckTrue(aVaule2.HasValue,'"aVaule2" has no value');
+  // Guard.CheckInheritsFrom(aVaule1.Value, TComponent, '');
+  // Guard.RaiseArgumentFormatException('Aaaaa');
+end;
 
+procedure TMyTestObject.Test_Tuples;
+var
+  aTupleISD: Tuple<Integer, String, Double>;
+  aKeyValue: Tuple<String, Variant>;
+begin
   aTupleISD.Create(1, 'Ala ma kota', 22.5);
+  aKeyValue := Tuple<String, Variant>.Create('contacts','{"aaa":1, "bbb":2.0}');
 
+  Assert.AreEqual('Ala ma kota',aTupleISD.Value2);
+  Assert.AreEqual(22.5,aTupleISD.Value3,0.000001);
+  Assert.AreEqual('contacts',aKeyValue.Value1);
+end;
+
+procedure TMyTestObject.Test_Array;
+var
+  arr1: TArray<Integer>;
+  arr2: TArray<Integer>;
+begin
   arr1 := TArray.Copy<Integer>([5, 4, 3, 1, 2]);
   arr2 := TArray.Copy<Integer>(arr1);
   TArray.Sort<Integer>(arr2);
-  Assert.AreEqual(3, arr2[2]);
 
+  Assert.AreEqual(3, arr2[2]);
+end;
+
+procedure TMyTestObject.Test_Vector;
+var
+  aIntVect: Vector<Integer>;
+  aVect2: TIntVector;
+begin
   aIntVect.Add(1);
   aIntVect.Add([2, 3, 4, 5]);
   aIntVect.Add([6, 7, 8, 9, 10]);
   aVect2 := aIntVect.Slice(5, 3);
+
   Assert.AreEqual('[6, 7, 8]', aVect2.ToString);
 end;
 
