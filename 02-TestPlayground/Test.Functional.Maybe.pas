1098f5df12425ed959efa6bc1aa32324989440bd
Bogdan Polak <bogdan.polak@bsc.com.pl>
2020-09-13 22:03:31 +0200

    Grouped unit tests

--- 02-TestPlayground/Test.Functional.Maybe.pas
+++ 02-TestPlayground/Test.Functional.Maybe.pas
@@ -15,14 +15,13 @@ type
     // [TearDown] procedure TearDown;
   published
     procedure HasValue;
-    procedure Assign99;
-    procedure TestException;
-    procedure MaybeFromVariant;
-    procedure TestEquals;
-    procedure TestAssignFloat;
-    procedure TestAssignStringInt;
-    procedure AssignSetOf;
-    procedure AssignNoValue;
+    procedure AssignPrimitiveValues;
+    procedure NoValue;
+    procedure Equals_OneToAnother;
+
+    procedure Exception_InvalidCast_WhenFloatToWord;
+    procedure Exception_InvalidCast_WhenNumerciStringToInteger;
+    procedure Exception_InvalidOperation_WhenGetNoValue;
   end;
 
 implementation
@@ -35,98 +34,92 @@ uses
 
 procedure TestFunctionalMaybe.HasValue;
 var
-  maybeString: Maybe<String>;
+  maybeEmptyString: Maybe<String>;
 begin
-  Assert.IsTrue(maybeString.HasValue = False);
-  maybeString:= '';
-  Assert.IsTrue(maybeString.HasValue);
+  Assert.IsFalse(maybeEmptyString.HasValue);
+  maybeEmptyString:= '';
+  Assert.IsTrue(maybeEmptyString.HasValue);
 end;
 
-procedure TestFunctionalMaybe.Assign99;
+procedure TestFunctionalMaybe.NoValue;
+var
+  maybeIntegerNull: Maybe<Integer>;
+  maybeCurrencyNull: Maybe<Currency>;
+begin
+  maybeCurrencyNull := Maybe<Currency>.Create(System.Variants.Null);
+  maybeIntegerNull := 11;
+  maybeIntegerNull := nil;
+
+  Assert.IsFalse(maybeIntegerNull.HasValue);
+  Assert.IsFalse(maybeCurrencyNull.HasValue);
+end;
+
+procedure TestFunctionalMaybe.AssignPrimitiveValues;
+var
+  maybeInteger99: Maybe<Integer>;
+  variant111_99: Variant;
+  maybeSetEmpty: Maybe<TReplaceFlags>;
+  maybeSetIgnoreCase: Maybe<TReplaceFlags>;
+  maybeCurrency111_99: Maybe<Currency>;
+begin
+  variant111_99 := 111.99;
+
+  maybeInteger99 := 99;
+  maybeCurrency111_99 := Maybe<Currency>.Create(variant111_99);
+  maybeSetEmpty := [];
+  maybeSetIgnoreCase := [rfIgnoreCase];
+
+  Assert.AreEqual(99, maybeInteger99.Value);
+  Assert.IsTrue(maybeSetIgnoreCase.Value = [rfIgnoreCase]);
+  Assert.IsTrue(maybeSetIgnoreCase = [rfIgnoreCase]);
+  Assert.IsTrue(maybeSetEmpty.HasValue);
+  Assert.IsTrue(maybeCurrency111_99.HasValue);
+  Assert.AreEqual(111.99, maybeCurrency111_99.Value, 0.00001);
+  Assert.IsTrue(maybeCurrency111_99 = 111.99);
+end;
+
+procedure TestFunctionalMaybe.Equals_OneToAnother;
+var
+  maybeNullString1: Maybe<String>;
+  maybeNullString2: Maybe<String>;
+  maybeStringAAA1: Maybe<String>;
+  maybeStringAAA2: Maybe<String>;
+  maybeStringBBB2: Maybe<String>;
+begin
+  maybeStringAAA1 := 'AAA';
+  maybeStringAAA2 := 'AAA';
+  maybeStringBBB2 := 'BBB';
+
+  Assert.IsTrue(maybeNullString1.Equals(maybeNullString2));
+  Assert.IsTrue(maybeStringAAA1.Equals(maybeStringAAA2));
+  Assert.IsFalse(maybeStringAAA1.Equals(maybeStringBBB2));
+end;
+
+procedure TestFunctionalMaybe.Exception_InvalidCast_WhenFloatToWord;
+var
+  maybeWord: Maybe<Word>;
+begin
+  Assert.WillRaise(procedure begin
+    maybeWord := Maybe<Word>(99.9);
+  end, EInvalidCast);
+end;
+
+procedure TestFunctionalMaybe.Exception_InvalidCast_WhenNumerciStringToInteger;
 var
   maybeInteger: Maybe<Integer>;
 begin
-  maybeInteger := 99;
-  Assert.AreEqual(99, maybeInteger.Value);
+  Assert.WillRaise(procedure begin
+    maybeInteger := Maybe<Integer>('111');
+  end, EInvalidCast);
 end;
 
-procedure TestFunctionalMaybe.AssignNoValue;
+procedure TestFunctionalMaybe.Exception_InvalidOperation_WhenGetNoValue;
 var
-  maybeInteger: Maybe<Integer>;
+  maybeStringList: Maybe<TStringList>;
 begin
-  maybeInteger := 11;
-  maybeInteger := nil;
-  Assert.IsTrue(not maybeInteger.HasValue);
-end;
-
-procedure TestFunctionalMaybe.TestAssignFloat;
-begin
-  // ExpectedException := EInvalidCast;
-  // fInteger := Maybe<Integer>(99.9);
-  Assert.Pass();
-end;
-
-procedure TestFunctionalMaybe.TestAssignStringInt;
-begin
-  // Nullable does NOT do a variant type conversion but is strict about the underlying type
-  // ExpectedException := EInvalidCast;
-  // fInteger := Maybe<Integer>('5');
-  Assert.Pass();
-end;
-
-procedure TestFunctionalMaybe.TestException;
-begin
-  // ExpectedException := EInvalidOperationException;
-  // fInteger.Value;
-  Assert.Pass();
-end;
-
-procedure TestFunctionalMaybe.AssignSetOf;
-var
-  maybeSet: Maybe<TReplaceFlags>;
-begin
-  maybeSet := [rfIgnoreCase];
-  Assert.IsTrue(maybeSet = [rfIgnoreCase]);
-  maybeSet := nil;
-  Assert.IsTrue(maybeSet.HasValue = False);
-end;
-
-procedure TestFunctionalMaybe.MaybeFromVariant;
-var
-  maybeInteger: Maybe<Integer>;
-  value: Variant;
-const
-  ExpectedInteger: Integer = 999;
-begin
-  maybeInteger := Maybe<Integer>.Create(value);
-  Assert.IsTrue(maybeInteger.HasValue = False);
-  maybeInteger := System.Variants.Null;
-  Assert.IsTrue(maybeInteger.HasValue = False);
-  value := ExpectedInteger;
-  maybeInteger := Maybe<Integer>.Create(value);
-  Assert.IsTrue(maybeInteger.HasValue);
-  Assert.AreEqual(ExpectedInteger, maybeInteger.Value);
-end;
-
-procedure TestFunctionalMaybe.TestEquals;
-var
-  a, b: Maybe<Integer>;
-begin
-  Assert.IsFalse(a.HasValue);
-  Assert.IsFalse(b.HasValue);
-
-  Assert.IsTrue(a.Equals(b));
-  Assert.IsTrue(b.Equals(a));
-
-  a := 2;
-  Assert.IsFalse(a.Equals(b));
-  Assert.IsFalse(b.Equals(a));
-
-  b := 2;
-  Assert.IsTrue(a.Equals(b));
-
-  b := 3;
-  Assert.IsFalse(a.Equals(b));
+  Assert.WillRaise(procedure begin
+    maybeStringList.Value.Add('aaaa');
+  end, EInvalidOpException);
 end;
 
 end.
